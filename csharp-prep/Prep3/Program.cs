02efc24b58e01bfcd7affa35f83a6f8217e5256e
espadaone <espadapablo@gmail.com>
2023-04-27 00:08:30 -0300

    Added prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,40 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
+    
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        
+        string high="Higher";
+        string low="Lower";
+        string guess="You guessed it!";
+    
+    
+        
+        Random randomGenerator = new Random();
+        int y = randomGenerator.Next(1,101);
+        
+        int x = -1;
+        
+        while ( x !=y)
+        {   
+            Console.Write("What is your guess? ");
+            string valueFromguess = Console.ReadLine();
+            x = int.Parse(valueFromguess);
+
+            if (x < y)
+            {
+                Console.WriteLine($"{low}");
+            }
+            else if (x > y)
+            {
+                Console.WriteLine($"{high}.");
+            }
+            else
+            {
+                Console.WriteLine($"{guess}");
+            }
+        }
+    } 
+    
 }
